79e8afebedfb9bdc89deb426e2687c938a13f32b
Jonathan Norris <jonathan@taplytics.com>
2025-10-14 15:38:53 -0400

    chore: cleanup comment

--- src/target.rs
+++ src/target.rs
@@ -75,7 +75,6 @@ pub(crate) struct TargetDistribution {
 pub(crate) struct TargetAndHashes {
     pub(crate) target: Target,
     pub(crate) bounded_hash: murmurhash::BoundedHash,
-    /// Whether the user qualified for this target via a rollout
     pub(crate) is_rollout: bool,
 }
 
